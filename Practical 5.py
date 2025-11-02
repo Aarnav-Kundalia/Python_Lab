46b814866c6cc4e4e75013f6efd7026b22c7f779
Aarnav-Kundalia <24bsp01@spt.pdpu.ac.in>
2025-11-03 03:49:55 +0530

    Editted Practical 5 file as per template

--- Practical 5.py
+++ Practical 5.py
@@ -3,3 +3,9 @@ factorial = 1
 for i in range(1, n_num + 1):
     factorial *= i
 print(f"The factorial of {n_num} is {factorial}")
+
+n_num = int(input("Enter a number: "))
+for _ in range(n_num):
+    print(f"Current value: {n_num}")
+    n_num -= 1
+print(f"Final value: {n_num}")
